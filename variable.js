651919d6c7cac534eaff54879633d0cfeea2adf1
jami <abdulrehmanjami73@gmail.com>
2022-04-22 12:46:44 +0500

    variable

---
+++ variable.js
@@ -0,0 +1,8 @@
+// Variables are containers for storing data (storing data values).
+
+// In this example, x, y, and z, are variables, declared with the var keyword:
+var a=120;
+var x = 5;
+var y = 6;
+var z = x + y+a;
+console.log(z);
